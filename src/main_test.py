fb25e041a3b68ee1ddada102c51c27aec9255b89
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-07-31 14:20:10 +0100

    Added testing script

---
+++ src/main_test.py
@@ -0,0 +1,16 @@
+print('Preparing...')
+from tensorflow.keras.models import load_model
+from constants import BASELINE_MODEL_PATH, GLOBAL_SEED
+from loading import loadAugmentedBinaryDatasetFromFiles
+
+print('Loading dataset...')
+_, _, _, _, X_test, y_test = loadAugmentedBinaryDatasetFromFiles()
+
+print('Loading model...')
+model = load_model(BASELINE_MODEL_PATH)
+
+print('Evaluating model...')
+results = model.evaluate(X_test, y_test, batch_size=4, return_dict=True)
+
+print('Results:')
+print(results)
