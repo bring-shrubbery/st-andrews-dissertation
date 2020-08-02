439331da95fbdef629739e30711fe0b64426d8e6
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-08-02 15:24:45 +0100

    More constants!

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,5 @@
 DATASET_ROOT_DIRECTORY = '/cs/scratch/as521/data'
 GLOBAL_SEED = 123
 USERNAME = 'as521'
+MODEL_CHECKPOINT_DIR = "/cs/scratch/as521/models/checkpoints/"
 BASELINE_MODEL_PATH = "/cs/scratch/as521/models/checkpoints/baseline-model.h5"
