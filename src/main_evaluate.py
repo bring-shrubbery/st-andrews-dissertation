a674bce8e0a5aa086e6e17c8f85a748a207b5b9f
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-08-02 19:52:27 +0100

    Added confusion matrix to the evaluation script

--- src/main_evaluate.py
+++ src/main_evaluate.py
@@ -3,6 +3,7 @@ import os
 import sys
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from tensorflow.keras.models import load_model
+from tensorflow.math import confusion_matrix
 from constants import MODEL_CHECKPOINT_DIR, GLOBAL_SEED
 from loading import loadAugmentedBinaryDatasetFromFiles
 
@@ -20,23 +21,38 @@ model = load_model(full_path)
 
 print('Evaluating model on validation dataset...')
 results = model.evaluate(X_val, y_val, batch_size=4, return_dict=True, verbose=0)
+predictions = model.predict(X_val, batch_size=4, verbose=0)
+predictions = [round(p[0]) for p in predictions]
+conf_matrix = confusion_matrix(y_val, predictions)
 
 # Define print results function
 def printResults(res):
     for metric in res:
         print("  {}: {}".format(metric, res[metric]))
 
+def printConfusionMatrix(cf):
+    print(cf.numpy())
+
 print('Validation Results:')
 printResults(results)
+printConfusionMatrix(conf_matrix)
 
 print('Evaluating model on training dataset...')
 results = model.evaluate(X_train, y_train, batch_size=4, return_dict=True, verbose=0)
+predictions = model.predict(X_train, batch_size=2, verbose=0)
+predictions = [round(p[0]) for p in predictions]
+conf_matrix = confusion_matrix(y_train, predictions)
 
 print('Training Results:')
 printResults(results)
+printConfusionMatrix(conf_matrix)
 
 print('Evaluating model on testing dataset...')
 results = model.evaluate(X_test, y_test, batch_size=4, return_dict=True, verbose=0)
+predictions = model.predict(X_test, batch_size=2, verbose=0)
+predictions = [round(p[0]) for p in predictions]
+conf_matrix = confusion_matrix(y_test, predictions)
 
 print('Testing Results:')
 printResults(results)
+printConfusionMatrix(conf_matrix)
