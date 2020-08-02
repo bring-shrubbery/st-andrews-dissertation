d74171a1f2d1cd125989a219c4886c8c60a9e1ea
bring-shrubbery <antoni.silvestrovic@gmail.com>
2020-08-02 15:25:28 +0100

    Updated vgg16 script

--- src/vgg16.py
+++ src/vgg16.py
@@ -1,141 +1,140 @@
-# System imports.
+print('Preparing...')
+
+# System imports
 import sys
 import os
 import datetime
 
-# ML imports.
+# ML imports
 import numpy as np
-import tensorflow as tf
 from tensorflow.math import confusion_matrix
-import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
+from tensorflow.keras.models import load_model
+from tensorflow import random
+from tensorflow.data import Dataset
 
-# Local imports.
-from loading import loadAugmentedBinaryDataset
-from constants import GLOBAL_SEED
+# Local imports
+from loading import loadAugmentedBinaryDataset, loadAugmentedBinaryDatasetFromFiles
 from createVGG16 import createVGG16
+from plot_history import plotAndSave
+from constants import GLOBAL_SEED, BASELINE_MODEL_PATH
 
-# Take note of the starting time of the program.
-timestamp = datetime.datetime.now()
+from twitter_api import tweet
 
-# Load dataset
-X_train, y_train, X_val, y_val, X_test, y_test = loadAugmentedBinaryDataset()
+try:
+    # Setup constants
+    EPOCHS = 100
+    BATCH_SIZE = 2
+    LEARNING_RATE = 0.00001
+    DROPOUT_FACTOR = 0.3
+    L2_FACTOR = 0.1
 
-# Normalise the images of all three datasets.
-X_train = tf.keras.utils.normalize(X_train)
-X_val = tf.keras.utils.normalize(X_val)
-X_test = tf.keras.utils.normalize(X_test)
+    VERBOSITY_1 = 2
+    VERBOSITY_2 = 1
 
-# Set global seed for consistent run-to-run results.
-tf.random.set_seed(GLOBAL_SEED)
+    # Take note of the starting time of the program.
+    timestamp = datetime.datetime.now()
 
-# Create the model.
-model = createVGG16(
-    learning_rate=1e-5,
-    l2_factor=0.01,
-    dropout_factor=0.4
-)
+    # Load dataset
+    print('Loading dataset...')
+    X_train, y_train, X_val, y_val, X_test, y_test = loadAugmentedBinaryDatasetFromFiles()
 
-# Print the summary of the network.
-print(model.summary())
+    # Set global seed for consistent run-to-run results.
+    random.set_seed(GLOBAL_SEED)
 
-# Setup checkpoint callback
-checkpoint_path = "/cs/scratch/as521/models/checkpoints/basic-model-cp-{epoch:04d}.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
+    # Setup callbacks
+    earlyStopping = EarlyStopping(
+        monitor='val_loss',
+        patience=15,
+        verbose=VERBOSITY_2,
+        mode='min'
+    )
+    mcp_save = ModelCheckpoint(
+        BASELINE_MODEL_PATH,
+        save_best_only=True,
+        monitor='val_loss',
+        mode='min',
+        patience=10,
+        verbose=VERBOSITY_2
+    )
+    reduce_lr_loss = ReduceLROnPlateau(
+        monitor='val_loss',
+        factor=0.1,
+        patience=7,
+        verbose=VERBOSITY_2,
+        epsilon=1e-4,
+        mode='min'
+    )
+    csv_logger = CSVLogger('./baseline_log.csv')
 
-# Create a callback that saves the model's weights
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_path,
-    save_weights_only=True,
-    verbose=1,
-    period=25)
+    # Create the model.
+    print('Creating the model...')
+    model = createVGG16(
+        learning_rate=LEARNING_RATE,
+        l2_factor=L2_FACTOR,
+        dropout_factor=DROPOUT_FACTOR
+    )
 
-# Train the model, or load the weights.
-if "--load" in sys.argv:
-    # checkpoint_name = os.path.join(checkpoint_dir, sys.argv[sys.argv.index("--load") + 1])
-    checkpoint_name = tf.train.latest_checkpoint(checkpoint_dir)
-    model.load_weights(checkpoint_name)
-else:
+    # Print the summary of the network.
+    print(model.summary())
+
     # Train the model.
-    history = model.fit(X_train, y_train, epochs=50, batch_size=3,
-                        validation_data=(X_val, y_val), callbacks=[], verbose=2)
+    print('Training the model...')
+    history = model.fit(
+        X_train,
+        y_train,
+        # train_dataset,
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+        validation_data=(X_val, y_val),
+        # validation_data=val_dataset,
+        callbacks=[earlyStopping, mcp_save, reduce_lr_loss, csv_logger],
+        verbose=VERBOSITY_1
+    )
 
-    # Create the figure.
-    f = plt.figure(figsize=(15, 10))
-    f.tight_layout()
+    plotAndSave('./baseline_results.png', history, model, X_val, y_val)
 
-    # Create subplots for each metric and confusion matrix.
-    ax1 = f.add_subplot(231, xlabel='Epoch', ylabel='Accuracy',  ylim=[0, 1])
-    ax2 = f.add_subplot(232, xlabel='Epoch', ylabel='MSE',       ylim=[0, 1])
-    ax3 = f.add_subplot(233, xlabel='Epoch', ylabel='Precision', ylim=[0, 1])
-    ax4 = f.add_subplot(234, xlabel='Epoch', ylabel='Recall',    ylim=[0, 1])
-    ax5 = f.add_subplot(235, xlabel='Epoch', ylabel='Loss')
-    ax6 = f.add_subplot(236, xlabel='Predicted', ylabel='Actual')
-
-    ax1.plot(history.history['accuracy'], label='accuracy')
-    ax1.plot(history.history['val_accuracy'], label='val_accuracy')
-    ax1.legend(loc='lower right')
-
-    ax2.plot(history.history['mse'], label='mse')
-    ax2.plot(history.history['val_mse'], label='val_mse')
-    ax2.legend(loc='lower right')
-
-    ax3.plot(history.history['precision'], label='precision')
-    ax3.plot(history.history['val_precision'], label='val_precision')
-    ax3.legend(loc='lower right')
-
-    ax4.plot(history.history['recall'], label='recall')
-    ax4.plot(history.history['val_recall'], label='val_recall')
-    ax4.legend(loc='lower right')
-
-    ax5.plot(history.history['loss'], label='loss')
-    ax5.plot(history.history['val_loss'], label='val_loss')
-    ax5.legend(loc='lower right')
-
-    # Make validation predictions and construct confusion matrix.
-    predictions = model.predict(X_val, batch_size=2)
+    print('Evaluating results...')
+    # Generate confusion matrix again, for evaluation and training dataset.
+    cf_text = ""
+    predictions = model.predict(X_val, batch_size=BATCH_SIZE)
     predictions = [round(p[0]) for p in predictions]
-    print("Predictions:", predictions)
-    print("Y_val:", y_val)
     cf = confusion_matrix(y_val, predictions)
-    cf = np.array(cf)
+    cf_text += "Validation\n" + str(cf.numpy())[1:-1].replace('\n ', '\n') + "\n"
 
-    # Plot confusion matrix.
-    im_id = ax6.imshow(cf, interpolation='nearest')
-    ax6.set_xticks(np.arange(2))
-    ax6.set_yticks(np.arange(2))
-    ax6.set_xticklabels(['positive', 'negative'])
-    ax6.set_yticklabels(['positive', 'negative'])
-    ax6.set_title("Confusion Matrix")
-    f.colorbar(im_id)
-    plt.setp(ax6.get_yticklabels(), rotation=90, ha="center", va="baseline",
-            rotation_mode="anchor")
+    predictions = model.predict(X_train, batch_size=BATCH_SIZE)
+    predictions = [round(p[0]) for p in predictions]
+    cf = confusion_matrix(y_train, predictions)
+    cf_text += "Training\n" + str(cf.numpy())[1:-1].replace('\n ', '\n') + "\n"
 
-    for i in range(2):
-        for j in range(2):
-            text = ax6.text(j, i, cf[i, j], ha="center",
-                            va="center", color="w")
+    # Save confusion matrices in text format into the file.
+    file = open('vgg16_confusion_matrix.txt', 'w')
+    file.write(cf_text)
+    file.close()
 
-    # Save the figure as an image.
-    f.savefig('./results_vgg16_{}.png'.format(lr))
+    print(cf_text)
 
-# Generate confusion matrix again, for evaluation and training dataset.
-cf_text = ""
-predictions = model.predict(X_val, batch_size=2)
-predictions = [round(p[0]) for p in predictions]
-cf = confusion_matrix(y_val, predictions)
-cf_text += "Validation\n" + str(cf) + "\n"
+    # Print total execution time.
+    dat_dur = datetime.datetime.now() - timestamp
+    print("Total execution time: ", dat_dur)
 
-predictions = model.predict(X_train, batch_size=2)
-predictions = [round(p[0]) for p in predictions]
-cf = confusion_matrix(y_train, predictions)
-cf_text += "Training\n" + str(cf) + "\n"
+    print('Final evalutaiton...')
+    evaluation = model.evaluate(
+        X_val, y_val, batch_size=BATCH_SIZE, verbose=VERBOSITY_1, return_dict=True)
+    print(evaluation)
+    val_acc = int(evaluation['accuracy']*100)
 
-# Save confusion matrices in text format into the file.
-file = open('confusion_matrix_vgg16_{}.txt'.format(lr), 'w')
-file.write(cf_text)
-file.close()
+    duration = dat_dur.seconds // 60  # Minutes (rounded)
 
-print(cf_text)
+    if duration > 60:
+        duration = str(int(duration / 6) / 10) + ' hrs'
+    else:
+        duration = str(int(duration * 10) / 10) + ' mins'
 
-# Print total execution time.
-print("Total execution time: ", datetime.datetime.now() - timestamp)
+    tweet("@bring_shrubbery \nDone.\nTime: {}\nAcc: .{}\n{}".format(
+        duration,
+        val_acc,
+        cf_text
+    ), "baseline_results.png")
+except:
+    tweet("@bring_shrubbery Latest execution crashsed at {}".format(datetime.datetime.now()))
